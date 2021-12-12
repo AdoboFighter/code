8de43ff3dd71943440e1ddecff37f5b27d79c8a4
adoboighter <hilairon123@gmail.com>
2021-12-12 22:23:53 +0800

    Manipulation of views

--- firstProject/routes/web.php
+++ firstProject/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index'])->name('products');
+Route::get('/about', [PagesController::class, 'about']);
+
