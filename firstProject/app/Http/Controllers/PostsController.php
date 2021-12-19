93873f8754bef77dd0edd10478a45fdb4746ca9f
adoboighter <hilairon123@gmail.com>
2021-12-19 17:16:13 +0800

    maybe last for this lessons (query builder)

--- firstProject/app/Http/Controllers/PostsController.php
+++ firstProject/app/Http/Controllers/PostsController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        //Non fluent
-        //DB:select([])
-        return view('posts/index');
+        $id = 2;
+
+        $posts = DB::table('posts')
+            ->select('*')
+            ->get();
+
+        dd($posts);
     }
 }
